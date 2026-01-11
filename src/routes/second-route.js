799dfb7973a83f5cbe05c0ea53b082eaa7cc9268
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-01-11 19:31:13 +0100

    update

--- src/routes/second-route.js
+++ src/routes/second-route.js
@@ -1,9 +1,11 @@
 export default async function routes(fastify, options) {
 	fastify.get('/02', async (request, reply) => {
-		return { message: 'This is the second route' };
+		return reply.view('results', {
+			results: [],
+		});
 	});
 
 	fastify.get('/03', async (request, reply) => {
-		return { info: 'This is the third route' };
+		return reply.view('index', {});
 	});
 }
