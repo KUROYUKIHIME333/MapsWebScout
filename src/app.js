c0ac729b7512f131a28c8e7e69198b2bb218c961
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-01-11 11:56:57 +0100

    Run a server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { Fastify } from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+// First route , to begin with
+fastify.get('/', async (request, reply) => {
+	return { hello: 'world' };
+});
+
+// Le serveur
+const start = async () => {
+	try {
+		await fastify.listen({ port: 3000 });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+};
+start();
