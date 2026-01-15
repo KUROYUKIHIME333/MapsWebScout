d44f90b42e2cd3f45f4b7f294d6671a7c66b8596
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-01-15 16:04:09 +0100

    Updates api urls

--- apiUrls.js
+++ apiUrls.js
@@ -1,3 +1,5 @@
 export const placeNearbyApiUrl = 'https://places.googleapis.com/v1/places:searchNearby';
+export const googleSheetApiUrl = 'https://www.googleapis.com/auth/spreadsheets';
+export const googleDriveApiUrl = 'https://www.googleapis.com/auth/drive';
 
 export const defaultCenterOfMaps = [15.307, -4.3224];
